eaaaad6a7b7b25cc89fdbfcd27a247b6517c006d
Samit26 <samitkhedekar2594@gmail.com>
2024-07-18 11:58:18 +0530

    new

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+var tableContent = document.getElementsByClassName('table-contents');
+// let tableContent = document.getElementsByClassName('table-contents');
 
 
+function openTab(tableName) {
+    for (let i = 0; i < tableContent.length; i++) {
+        tableContent[i].classList.remove('active-table');
+    }
+    document.getElementById(tableName).classList.add('active-table');
+}
