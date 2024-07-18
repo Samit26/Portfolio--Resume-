7bc985a23632b724be6a63882a7a4868551253a8
Samit26 <samitkhedekar2594@gmail.com>
2024-07-18 12:20:13 +0530

    new1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var tableContent = document.getElementsByClassName('table-contents');
-// let tableContent = document.getElementsByClassName('table-contents');
+let tabLink = document.getElementsByClassName('tab-links');
 
 
 function openTab(tableName) {
@@ -7,4 +7,18 @@ function openTab(tableName) {
         tableContent[i].classList.remove('active-table');
     }
     document.getElementById(tableName).classList.add('active-table');
+
+    for (let i = 0; i < tabLink.length; i++) {
+        tabLink[i].classList.remove('active-list');
+    }
+
+    if(tableName == 'skill'){
+        document.getElementById('skill-1').classList.add('active-list');
+    }
+    if(tableName == 'experience'){
+        document.getElementById('experience-1').classList.add('active-list');
+    }
+    if(tableName == 'education'){
+        document.getElementById('education-1').classList.add('active-list');
+    }
 }
