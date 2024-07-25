ae17bbfa3adf495877a872ce5b3fe23be3dbc79d
Samit26 <samitkhedekar2594@gmail.com>
2024-07-25 22:23:26 +0530

    new update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var tableContent = document.getElementsByClassName('table-contents');
 let tabLink = document.getElementsByClassName('tab-links');
 
 
+
 function openTab(tableName) {
     for (let i = 0; i < tableContent.length; i++) {
         tableContent[i].classList.remove('active-table');
@@ -22,3 +23,50 @@ function openTab(tableName) {
         document.getElementById('education-1').classList.add('active-list');
     }
 }
+
+const navBtn = (id) => {
+    let navbarA = document.getElementsByClassName('navbar-a');
+    let element = document.getElementById(id);
+    for (let i = 0; i < navbarA.length; i++) {
+        navbarA[i].classList.remove('active-nav');
+    }
+    if (element) { 
+        // element.classList.add('active-nav');
+        element.scrollIntoView({behavior: "smooth" });
+    } else {
+        console.error('Element with ID ' + id + ' not found.');
+    }
+
+    if(id == 'home'){
+        document.getElementById('home-1').classList.add('active-nav');
+    }
+    if(id == 'about'){
+        document.getElementById('about-1').classList.add('active-nav');
+    }
+    if(id == 'project'){
+        document.getElementById('project-1').classList.add('active-nav');
+    }
+    if(id == 'contact'){
+        document.getElementById('contact-1').classList.add('active-nav');
+    }
+};
+
+// Listen for scroll events on the window
+window.addEventListener('scroll', function() {
+    // Get the About section and navbar element by ID
+    var aboutSection = document.getElementById('about');
+    var aboutNav = document.getElementById('about-1');
+
+    // Calculate the position of the About section relative to the viewport
+    var rect = aboutSection.getBoundingClientRect();
+
+    // Check if the About section is in view
+    if(rect.top >= 0 && rect.bottom <= window.innerHeight) {
+        // About section is in view, add the active class
+        aboutNav.classList.add('active-nav');
+    } else {
+        // About section is not in view, remove the active class
+        aboutNav.classList.remove('active-nav');
+    }
+});
+
